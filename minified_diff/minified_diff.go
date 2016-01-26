49d0e358cda1030c309fae9c09415e70bb30d74b
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-01-26 13:54:47 +0900

    Use jsbeautifier.BeatifiFile

--- minified_diff/minified_diff.go
+++ minified_diff/minified_diff.go
@@ -1,6 +1,13 @@
 package minified_diff
 
+import (
+	"github.com/ditashi/jsbeautifier-go/jsbeautifier"
+)
+
 func MinifiedDiff(file1 string, file2 string) *string {
-	result := file1 + " " + file2
+	options := jsbeautifier.DefaultOptions()
+	result1 := jsbeautifier.BeautifyFile(file1, options)
+	result2 := jsbeautifier.BeautifyFile(file2, options)
+	result := *result1 + " " + *result2
 	return &result
 }
